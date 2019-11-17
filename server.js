790562dd0ce1121ede234e0a012e031595916031
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-11-17 22:17:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(helmet());
+
 //Index page (static HTML)
 app.route('/').get(function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
